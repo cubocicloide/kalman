0e61ddc8aa3c42908a06c853dfa4e41e69c4025c
=cubocicloide <=f.sasso333@gmail.com>
2018-05-18 16:36:26 +0200

    pred

--- main.cpp
+++ main.cpp
@@ -8,40 +8,15 @@ Date:       03 May 2018
 #include <eigen3/Eigen/Dense>
 #include <iostream>
 #include <math.h>
-#include <pthread.h>
 #include <random>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 #include <typeinfo>
 #include <unistd.h>
 
-/* CONSTANTS */
-#define COL_CYAN                3
-#define COL_GRAY                7
-#define COL_GREEN               10
-#define COL_RED                 4
-
-#define DISPLAY_PERIOD          10000000 // 0.01 [sec] (given in nanosecond)
-#define KEYBOARD_PERIOD         10000000 // 0.01 [sec] (given in nanosecond)
-
-#define KALMAN_PERIOD           10000000 // 0.01 [sec] (given in nanosecond)
-#define FRAME_PER_SECOND        100      // (1 / 0.01) [1 / sec]
-#define PREDICT_AMOUNT          0.5      // 0.5 [sec]
-
-#define MAX_NOISE               10
-#define MIN_NOISE               1
-
-#define MAX_TRAIL_LENGTH        150
-#define MIN_TRAIL_LENGTH        20
-
-#define MEASUREMENT_LENGHT      20
-
-#define POINT_RADIUS            2
-
-#define WINDOW_HEIGHT           480
-#define WINDOW_WIDTH            640
+#include "ptask.h"
+#include "constants.h"
 
 /* NAMESPACES */
 using namespace std;
@@ -50,6 +25,7 @@ using namespace Eigen;
 /* GLOBAL VARIABLES */
 char s[100];
 float measurement[MEASUREMENT_LENGHT][2];
+float predict_amount;
 int trail[MAX_TRAIL_LENGTH][2];
 int prediction[2];
 int exit_flag;
@@ -60,6 +36,7 @@ int trail_length;
 pthread_mutex_t mux_measurement;
 pthread_mutex_t mux_noise;
 pthread_mutex_t mux_prediction;
+pthread_mutex_t mux_predict_amount;
 pthread_mutex_t mux_trail;
 pthread_mutex_t mux_trail_length;
 
@@ -67,7 +44,7 @@ pthread_t thread_display;
 pthread_t thread_kalman;
 pthread_t thread_keyboard;
 
-struct timespec t0, tx0;
+struct timespec t0;
 
 Matrix4f A, B, H, Q, R, I;
 Vector4f c;
@@ -94,11 +71,13 @@ void init() {
     noise = MIN_NOISE;
     exit_flag = 1;
     prediction_flag = 1;
+    predict_amount = 0.5;
 
     // mutex 
     mux_measurement = PTHREAD_MUTEX_INITIALIZER;
     mux_noise = PTHREAD_MUTEX_INITIALIZER;
     mux_prediction = PTHREAD_MUTEX_INITIALIZER;
+    mux_predict_amount = PTHREAD_MUTEX_INITIALIZER;
     mux_trail = PTHREAD_MUTEX_INITIALIZER;
     mux_trail_length = PTHREAD_MUTEX_INITIALIZER;
 
@@ -167,6 +146,8 @@ void init() {
     
     set_color_depth(8);
     set_gfx_mode(GFX_AUTODETECT_WINDOWED, WINDOW_WIDTH, WINDOW_HEIGHT, 0, 0);
+
+	// ptask_init(SCHED_FIFO);
 }
 
 // Implement the Kalman's filter algorithm
@@ -188,7 +169,7 @@ void kalman_iterate(Vector4f c, Vector4f m) {
     // If prediction is enabled, the prediction step is looped for FRAME_PER_SECOND * PREDICT_AMOUNT frames
     if (prediction_flag == 1) {
         prediction_x = x;
-        for (int i = 1; i < (int) FRAME_PER_SECOND * PREDICT_AMOUNT; i++) {
+        for (int i = 1; i < (int) FRAME_PER_SECOND * predict_amount; i++) {
             prediction_x = A * prediction_x + B * c;
         }
     }
@@ -219,7 +200,7 @@ void *display_task(void *arg_in) {
     period.tv_sec = 0;
     period.tv_nsec = DISPLAY_PERIOD; 
 
-    next_act = tx0;
+    next_act = t0;
 
     do {
         // exit the program if exit_flag is 0
@@ -252,14 +233,18 @@ void *display_task(void *arg_in) {
                 }
 
                 pthread_mutex_lock(&mux_prediction);
-                    if (prediction_flag == 1) {
-                        line(buffer, trail[MAX_TRAIL_LENGTH - 1][0], trail[MAX_TRAIL_LENGTH - 1][1], prediction[0], prediction[1], COL_RED);
-                        sprintf(s, "prediction enabled (press KEY_SPACE to disable it).");
-                        textout_ex(buffer, font, s, 5, 40, COL_CYAN, -1);
-                    } else {
-                        sprintf(s, "prediction disabled (press KEY_SPACE to enable it).");
-                        textout_ex(buffer, font, s, 5, 40, COL_CYAN, -1);
-                    }
+                    pthread_mutex_lock(&mux_predict_amount);
+                        if (prediction_flag == 1) {
+                            line(buffer, trail[MAX_TRAIL_LENGTH - 1][0], trail[MAX_TRAIL_LENGTH - 1][1], prediction[0], prediction[1], COL_RED);
+                            sprintf(s, "prediction enabled (press KEY_SPACE to disable it).");
+                            textout_ex(buffer, font, s, 5, 40, COL_CYAN, -1);
+                            sprintf(s, "prediction step: %.1f (press S/A to increase/decrease the prediction step).", predict_amount);
+                            textout_ex(buffer, font, s, 5, 56, COL_CYAN, -1);
+                        } else {
+                            sprintf(s, "prediction disabled (press KEY_SPACE to enable it).");
+                            textout_ex(buffer, font, s, 5, 40, COL_CYAN, -1);
+                        }
+                    pthread_mutex_unlock(&mux_predict_amount);
                 pthread_mutex_unlock(&mux_prediction);
 
                 sprintf(s, "trail's length: %i (press KEY_LEFT/KEY_RIGHT to increase/decrease it).", trail_length);
@@ -293,7 +278,7 @@ void *kalman_task(void *arg_in) {
     period.tv_sec = 0;
     period.tv_nsec = KALMAN_PERIOD; 
 
-    next_act = tx0;
+    next_act = t0;
 
     do {
         // get the last measurement and update the vector of measurements accordingly
@@ -320,15 +305,17 @@ void *kalman_task(void *arg_in) {
             m(3) = (float) (m(1) - measurement[MEASUREMENT_LENGHT - 2][1]) * 1000000000 / KALMAN_PERIOD;
 
             pthread_mutex_lock(&mux_prediction);
-                kalman_iterate(c, m);
+                pthread_mutex_lock(&mux_predict_amount);
+                    kalman_iterate(c, m);
 
-                trail[MAX_TRAIL_LENGTH - 1][0] = (int) x(0);    
-                trail[MAX_TRAIL_LENGTH - 1][1] = (int) x(1);
+                    trail[MAX_TRAIL_LENGTH - 1][0] = (int) x(0);    
+                    trail[MAX_TRAIL_LENGTH - 1][1] = (int) x(1);
 
-                if (prediction_flag == 1) {
-                    prediction[0] = (int) prediction_x(0);
-                    prediction[1] = (int) prediction_x(1);
-                }
+                    if (prediction_flag == 1) {
+                        prediction[0] = (int) prediction_x(0);
+                        prediction[1] = (int) prediction_x(1);
+                    }
+                pthread_mutex_unlock(&mux_predict_amount);
             pthread_mutex_unlock(&mux_prediction);
         pthread_mutex_unlock(&mux_trail);
 
@@ -350,7 +337,7 @@ void *keyboard_task(void *arg_in) {
     period.tv_sec = 0;
     period.tv_nsec = KEYBOARD_PERIOD; 
 
-    next_act = tx0;
+    next_act = t0;
 
     do {
         // get the key pressed by the user
@@ -392,10 +379,26 @@ void *keyboard_task(void *arg_in) {
                 exit_flag = - exit_flag;
                 break;
         }
+        switch (ascii) {
+            case 's':
+                pthread_mutex_lock(&mux_predict_amount);
+                    if (predict_amount < MAX_PREDICT_AMOUNT) {
+                        predict_amount = predict_amount + 0.1;
+                    }
+                pthread_mutex_unlock(&mux_predict_amount);
+                break;
+            case 'a':
+                pthread_mutex_lock(&mux_predict_amount);
+                    if (predict_amount > MIN_PREDICT_AMOUNT) {
+                        predict_amount = predict_amount - 0.1;
+                    }
+                pthread_mutex_unlock(&mux_predict_amount);
+                break;
+        }
 
         // update the next action variable and wait for the next period
-        timespec_add(&next_act, &period);
         clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &next_act, NULL);
+        timespec_add(&next_act, &period);
         
     } while (exit_flag == 1); 
     pthread_exit(NULL);
@@ -409,8 +412,7 @@ int main(){
     init();
 
     // initialize the clock
-    clock_gettime(CLOCK_REALTIME, &t0);     // "zero" time
-    clock_gettime(CLOCK_MONOTONIC, &tx0);
+    clock_gettime(CLOCK_MONOTONIC, &t0);
     
     // create the threads
     pthread_create(&thread_display, NULL, display_task, NULL);
